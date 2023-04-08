67921a7a139fe343c29c5dc207bde68815adebf8
Samantha Tobias <SamanthaArnoldussen@gmail.com>
2023-04-08 22:27:37 +0200

    log debug level and current time

--- core/src/nl/samanthatobias/voltling/utils/GdxLogger.java
+++ core/src/nl/samanthatobias/voltling/utils/GdxLogger.java
@@ -1,20 +1,23 @@
 package nl.samanthatobias.voltling.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class GdxLogger {
 
-	// TODO: log the time
-	// TODO: log the log level
+	private final static DateFormat format = new SimpleDateFormat("HH:mm:ss");
 
-	private final String tag;
+	private final String className;
 
 	public GdxLogger(Class<?> clazz) {
-		this.tag = clazz.getSimpleName();
+		this.className = clazz.getSimpleName();
 	}
 
 	public void info(String message) {
-		Gdx.app.log(tag, message);
+		Gdx.app.log(tag("LOG"), message);
 	}
 
 	public void info(String format, Object... args) {
@@ -22,7 +25,7 @@ public class GdxLogger {
 	}
 
 	public void error(String message) {
-		Gdx.app.error(tag, message);
+		Gdx.app.error(tag("ERR"), message);
 	}
 
 	public void error(String format, Object... args) {
@@ -30,11 +33,16 @@ public class GdxLogger {
 	}
 
 	public void debug(String message) {
-		Gdx.app.debug(tag, message);
+		Gdx.app.debug(tag("DBG"), message);
 	}
 
 	public void debug(String format, Object... args) {
 		debug(format(format, args));
+	}
+
+	private String tag(String logLevel) {
+		String time = format.format(TimeUtils.millis());
+		return logLevel + " " + time + " " + className;
 	}
 
 	private static String format(String message, Object... args) {
